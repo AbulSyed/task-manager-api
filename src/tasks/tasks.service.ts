5a5d1050b5190133fc0f7204c7f93b3846c2eb7e
Abul Syed <syed1234@hotmail.co.uk>
2021-08-02 22:33:53 +0100

    err handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -31,7 +31,13 @@ export class TasksService {
   }
 
   getTask(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const tasks = this.tasks.find(task => task.id === id);
+
+    if(!tasks){
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    return tasks;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -55,6 +61,10 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const task = this.getTask(id);
+
+    if(task){
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    }
   }
 }
