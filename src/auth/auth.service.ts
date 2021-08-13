62ea052b5f44beb90d58dcf9f4a2253b0bb27389
Abul Syed <syed1234@hotmail.co.uk>
2021-08-13 19:09:43 +0100

    jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,42 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { SigninUserDto } from './dto/signin-user.dto';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
-    private usersRepository: UsersRepository
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    return this.usersRepository.createUser(createUserDto);
+    const { username, password } = createUserDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword
+    })
+
+    try {
+      const newUser = await this.usersRepository.save(user);
+      delete newUser.password;
+      return newUser;
+    }catch(err){
+      if(err.code === '23505'){
+        throw new ConflictException('Username already in use');
+      }else{
+        throw new InternalServerErrorException();
+      }
+    }
   }
 
   async signin(signinUserDto: SigninUserDto): Promise<User> {
@@ -29,6 +51,10 @@ export class AuthService {
       throw new UnauthorizedException('Failed login');
     }
 
-    return user;
+    const token = await this.jwtService.sign({ id: user.id });
+    const payload = { ...user, token };
+    delete payload.password;
+
+    return payload;
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,30 +1,6 @@
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm"
-import { CreateUserDto } from "./dto/create-user.dto"
 import { User } from "./user.entity"
-import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const { username, password } = createUserDto;
-
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt)
-
-    const user = this.create({
-      username,
-      password: hashedPassword
-    })
-
-    try {
-      return await this.save(user);
-    }catch(err){
-      if(err.code === '23505'){
-        throw new ConflictException('Username already in use');
-      }else{
-        throw new InternalServerErrorException();
-      }
-    }
-  }
 }
