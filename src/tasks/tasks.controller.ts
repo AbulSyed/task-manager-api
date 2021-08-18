c216440cebacfce467a072aaedd29c8093390bbe
Abul Syed <syed1234@hotmail.co.uk>
2021-08-18 21:16:07 +0100

    mapping

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -12,8 +14,9 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-    return this.tasksService.createTask(createTaskDto);
+  createTask(@Body() createTaskDto: CreateTaskDto,
+  @GetUser() user: User): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto, user);
   }
 
   @Get()
--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,3 +1,4 @@
+import { User } from "src/auth/user.entity";
 import { Brackets, EntityRepository, Repository } from "typeorm";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
@@ -6,13 +7,14 @@ import { Task } from "./task.entity";
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, desc } = createTaskDto;
 
     const task = this.create({
       title,
       desc,
-      status: TaskStatus.OPEN
+      status: TaskStatus.OPEN,
+      user
     })
 
     return await this.save(task);
